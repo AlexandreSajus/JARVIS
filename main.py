aa49858482c93b1953a5882074d8b8805cf1ffdd
Alexandre Sajus <alexandre.sajus@student-cs.fr>
2023-12-15 17:45:36 +0100

    :tada: Functional dialogue with audio

---
+++ main.py
@@ -0,0 +1,94 @@
+import os
+from os import PathLike
+import asyncio
+from dotenv import load_dotenv
+from typing import List, Optional, TypeVar, Union, IO
+
+import openai
+from deepgram import Deepgram
+from gtts import gTTS
+import pygame
+from pygame import mixer
+
+from record import SpeechToText
+
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+DEEPGRAM_API_KEY = os.getenv("DEEPGRAM_API_KEY")
+RECORDING_PATH = "wavs/recording.wav"
+
+gpt_client = openai.Client(api_key=OPENAI_API_KEY)
+context = "You are Sam, a personal assistant created by Alex. Your answers should be limited to 1-2 sentences."
+
+mixer.init()
+
+
+def request_gpt(prompt: str) -> str:
+    """
+    Send a prompt to the GPT-3 API and return the response.
+
+    Args:
+        - state: The current state of the app.
+        - prompt: The prompt to send to the API.
+
+    Returns:
+        The response from the API.
+    """
+    response = gpt_client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": f"{prompt}",
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    return response.choices[0].message.content
+
+
+async def transcribe(
+    file_name: Union[Union[str, bytes, PathLike[str], PathLike[bytes]], int]
+):
+    """
+    Transcribe audio using Deepgram API.
+
+    Args:
+        - file_name: The name of the file to transcribe.
+
+    Returns:
+        The response from the API.
+    """
+    with open(file_name, "rb") as audio:
+        source = {"buffer": audio, "mimetype": "audio/wav"}
+        response = await deepgram.transcription.prerecorded(source)
+        return response["results"]["channels"][0]["alternatives"][0]["words"]
+
+
+if __name__ == "__main__":
+    while True:
+        # Record audio
+        SpeechToText()
+        # Transcribe audio
+        deepgram = Deepgram(DEEPGRAM_API_KEY)
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        words = loop.run_until_complete(transcribe(RECORDING_PATH))
+        string_words = " ".join(
+            word_dict.get("word") for word_dict in words if "word" in word_dict
+        )
+        print(f"USER: {string_words}")
+        # Get response from GPT-3
+        context += f"\nAlex: {string_words}\nSam: "
+        response = request_gpt(string_words)
+        print(f"AI: {response}")
+        context += response
+        # Convert response to audio
+        tts = gTTS(response)
+        tts.save("wavs/response.wav")
+        # Play response
+        sound = mixer.Sound("wavs/response.wav")
+        sound.play()
+        pygame.time.wait(int(sound.get_length() * 1000))
+        print("LOG: Response played")
---
+++ record.py
@@ -0,0 +1,99 @@
+import io
+import sys
+import typing
+import time
+import wave
+from pathlib import Path
+
+from rhasspysilence import WebRtcVadRecorder, VoiceCommand, VoiceCommandResult
+import pyaudio
+
+pa = pyaudio.PyAudio()
+
+
+def SpeechToText() -> None:
+    """
+    Records audio until silence is detected
+    Saves audio to wavs/tester.wav
+    """
+    recorder = WebRtcVadRecorder(
+        vad_mode=3,
+        silence_seconds=4,
+    )
+    recorder.start()
+    # file directory
+    wav_sink = "wavs/"
+    # file name
+    wav_filename = "recording"
+    if wav_sink:
+        wav_sink_path = Path(wav_sink)
+        if wav_sink_path.is_dir():
+            # Directory to write WAV files
+            wav_dir = wav_sink_path
+        else:
+            # Single WAV file to write
+            wav_sink = open(wav_sink, "wb")
+    voice_command: typing.Optional[VoiceCommand] = None
+    audio_source = pa.open(
+        rate=16000,
+        format=pyaudio.paInt16,
+        channels=1,
+        input=True,
+        frames_per_buffer=960,
+    )
+    audio_source.start_stream()
+    print("LOG: Listening...")
+
+    def buffer_to_wav(buffer: bytes) -> bytes:
+        """Wraps a buffer of raw audio data in a WAV"""
+        rate = int(16000)
+        width = int(2)
+        channels = int(1)
+
+        with io.BytesIO() as wav_buffer:
+            wav_file: wave.Wave_write = wave.open(wav_buffer, mode="wb")
+            with wav_file:
+                wav_file.setframerate(rate)
+                wav_file.setsampwidth(width)
+                wav_file.setnchannels(channels)
+                wav_file.writeframesraw(buffer)
+
+            return wav_buffer.getvalue()
+
+    try:
+        chunk = audio_source.read(960)
+        while chunk:
+            # Look for speech/silence
+            voice_command = recorder.process_chunk(chunk)
+
+            if voice_command:
+                _ = voice_command.result == VoiceCommandResult.FAILURE
+                # Reset
+                audio_data = recorder.stop()
+                if wav_dir:
+                    # Write WAV to directory
+                    wav_path = (wav_dir / time.strftime(wav_filename)).with_suffix(
+                        ".wav"
+                    )
+                    wav_bytes = buffer_to_wav(audio_data)
+                    wav_path.write_bytes(wav_bytes)
+                    print(wav_path)
+                    print("file saved")
+                    break
+                elif wav_sink:
+                    # Write to WAV file
+                    wav_bytes = core.buffer_to_wav(audio_data)
+                    wav_sink.write(wav_bytes)
+            # Next audio chunk
+            chunk = audio_source.read(960)
+
+    finally:
+        try:
+            audio_source.close_stream()
+            print("LOG: Recording Saved")
+        except Exception:
+            pass
+
+
+if __name__ == "__main__":
+    SpeechToText()
